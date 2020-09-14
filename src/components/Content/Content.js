a6d958dcc1b5181a2bfbcc4ac37cea7b479076c5
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2020-09-14 22:55:41 +0300

    0.0.1

---
+++ src/components/Content/Content.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Content = () => {
+    return (
+        <div>
+            <p>
+                Content
+            </p>
+        </div>
+    )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import s from './Header.module.css'
+export const Header = () => {
+    return (
+        <div className={s.header}>
+            <h1 className={s.logo}>PokeDex</h1>
+        </div>
+    )
+}
