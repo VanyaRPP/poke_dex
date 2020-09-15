f325ecd93d33298ae8a477ec177d7cf3cf4ec364
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2020-09-15 18:07:42 +0300

    0

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -8,15 +8,14 @@ export const Paginationn = ({pokeCount, onShowSizeChange, onChange}) => {
     return (
         <div className={s.container}>
             <Pagination
+                defaultCurrent={0}
                 showSizeChanger
                 pageSizeOptions={[10,20,50,100]}
                 defaultPageSize={20}
-                defaultCurrent={1}
                 total={pokeCount}
                 onShowSizeChange={onShowSizeChange}
                 onChange={onChange}
-            showTotal={(pokeCount, range) => `${range[0]}-${range[1]} of ${pokeCount} items`}
-            
+                showTotal={(pokeCount, range) => `${range[0]}-${range[1]} of ${pokeCount} Pokemon`}
             />
         </div>
     )
