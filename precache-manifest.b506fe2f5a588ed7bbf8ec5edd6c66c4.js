b545679240d6f9acbd63ee7c8573d642abfd03e1
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2020-09-19 02:28:23 +0300

    Updates

--- precache-manifest.b506fe2f5a588ed7bbf8ec5edd6c66c4.js
+++ precache-manifest.b506fe2f5a588ed7bbf8ec5edd6c66c4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77159d6633fae358050b22872ce47cc3",
+    "revision": "c7bee7f3043b7d2c2fbd3454a55ba5d0",
     "url": "/poke_dex/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/poke_dex/static/css/2.99a5358a.chunk.css"
   },
   {
-    "revision": "c49d349569b8ae5ffc49",
+    "revision": "c934d29d83182d37e25b",
     "url": "/poke_dex/static/css/main.321df839.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/poke_dex/static/js/2.9576ee4c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c49d349569b8ae5ffc49",
-    "url": "/poke_dex/static/js/main.45624214.chunk.js"
+    "revision": "c934d29d83182d37e25b",
+    "url": "/poke_dex/static/js/main.dc8c623f.chunk.js"
   },
   {
     "revision": "6a0019ab0b9e5c0815d2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/poke_dex/precache-manifest.62ad55be2bab8995ecf11228999b59d5.js"
+  "/poke_dex/precache-manifest.b506fe2f5a588ed7bbf8ec5edd6c66c4.js"
 );
 
 self.addEventListener('message', (event) => {
