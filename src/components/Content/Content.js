0ee32d63440c36e4a9e444bfb424f83c407f0b4f
Vanya <51020174+VanyaRPP@users.noreply.github.com>
2020-09-25 12:51:50 +0300

    1

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -5,6 +5,7 @@ import { Paginationn } from '../Pagination/Pagination'
 import { PokePage } from '../PokePage/PokePage'
 import { Searchh } from '../Search/Search'
 import { SelectorType } from '../SelectType/SelectType'
+import s from './Content.module.css'
 
 export const Content = () => {
     const [pokeCount, setpokeCount] = useState()
@@ -12,38 +13,38 @@ export const Content = () => {
     const [page_Number, setpage_Number] = useState(0)
     const [search, setSearch] = useState('')
     const [type, setType] = useState(false)
-    const [TypeUrl, setTypeUrl] = useState('')
-    const [Url, setUrl] = useState(`https://pokeapi.co/api/v2/pokemon?limit=${page_Size}&offset=${page_Number}`)
+    const [typeUrl, setTypeUrl] = useState('')
+    const [url, setUrl] = useState(`https://pokeapi.co/api/v2/pokemon?limit=${page_Size}&offset=${page_Number}`)
 
     useEffect(() => {
         type?
-        setUrl(TypeUrl):
+        setUrl(typeUrl):
         search===''?setUrl(`https://pokeapi.co/api/v2/pokemon?limit=${page_Size}&offset=${page_Number}`):
         setUrl('https://pokeapi.co/api/v2/pokemon?limit=1050&offset=0')
     })
 
-function onShowSizeChange(pageSize) {
-    setpage_Size(pageSize)
-}
-function onChange(pageNumber) {
-    setpage_Number(pageNumber === 1 ? 0 :pageNumber*page_Size)
-}
-function ontypeClick(value){
-    setType(true)
-    setTypeUrl(value)
-}
-function onclearTypeClick(){
-    setType(false)
-}
+    function onShowSizeChange(pageSize) {
+        setpage_Size(pageSize)
+    }
+    function onChange(pageNumber) {
+        setpage_Number(pageNumber === 1 ? 0 :pageNumber*page_Size)
+    }
+    function ontypeClick(value){
+        setType(true)
+        setTypeUrl(value)
+    }
+    function onclearTypeClick(){
+        setType(false)
+    }
 
     return (
         <div>
             <Searchh setSearch={setSearch}/>
-            <Collapse ghost style={{ borderRadius:'20px',border:'2px solid violet'}}>
+            <Collapse className={s.collaps} ghost>
                 <CollapsePanel header="Select Poke Type" key="1">
                     <SelectorType
-                    ontypeClick={ontypeClick}
-                    onclearTypeClick={onclearTypeClick}
+                        ontypeClick={ontypeClick}
+                        onclearTypeClick={onclearTypeClick}
                     />
                 </CollapsePanel>
             </Collapse>
@@ -55,12 +56,12 @@ function onclearTypeClick(){
                 />
             }
             <PokePage
-            setpokeCount={setpokeCount}
-            page_Size={page_Size}
-            page_Number={page_Number}
-            Url={Url}
-            Type={type}
-            search={search}
+                setpokeCount={setpokeCount}
+                page_Size={page_Size}
+                page_Number={page_Number}
+                Url={url}
+                Type={type}
+                search={search}
             />
             <BackTop>
                 <p>Up</p>
--- src/components/PokeInfoModal/PokeInfoModal.js
+++ src/components/PokeInfoModal/PokeInfoModal.js
@@ -4,40 +4,40 @@ import { Modal, Progress } from 'antd';
 
 
 export default function PokeInfoModal({ pName, pType, TYPE_COLORS, pImg, PokeAbilities, PokeHeight, PokeBaseStats, PokeStatsName}){
+  
   const [visible, setvisible] = useState(true)
 
-function handleCancel(){
-  setvisible(false)
-}
-    return (
-      <>
-        <Modal
-          width={700}
-          title={pName}
-          visible={visible}
-          onCancel={handleCancel}
-          footer={null}
-          style={{
-            fontFamily:'Ranchers, cursive',
-            fontSize:'40px'
-          }}
-        >
-          <div className={s.content}>
-            <div className={ s.imgAndStats }>
-              <div className={s.imgDiv}>
-                <img src={pImg} style={{height:`219px`}}/>
+  function handleCancel(){
+    setvisible(false)
+  }
+  return(
+    <Modal
+      width={700}
+      title={pName}
+      visible={visible}
+      onCancel={handleCancel}
+      footer={null}
+      style={{
+        fontFamily:'Ranchers, cursive',
+        fontSize:'40px'
+      }}
+    >
+      <div className={s.content}>
+        <div className={ s.imgAndStats }>
+          <div className={s.imgDiv}>
+            <img src={pImg} style={{height:`219px`}}/>
+          </div>
+          <div className={s.statsDiv}>
+            <div>
+              <Progress
+                trailColor='violet'
+                strokeLinecap="square" type="dashboard"
+                percent={PokeHeight*2} format={percent => `${PokeHeight/10} m`}
+                status={"normal"}
+              />
               </div>
-              <div className={s.statsDiv}>
-                <div>
-                  <Progress
-                  trailColor='violet'
-                  strokeLinecap="square" type="dashboard"
-                  percent={PokeHeight*2} format={percent => `${PokeHeight/10} m`}
-                  status={"normal"}
-                  />
-                </div>
-                <div style={{display:'flex',justifyContent:'space-between'}}>
-                  <div style={{minWidth:'90px', display:'flex', flexDirection: 'column',justifyContent:'space-between' }}>
+                <div className={s.div_stats}>
+                  <div className={s.stn}>
                     {PokeStatsName.map(psn=>(
                       <p style={{fontSize:'13px', margin:'0'}}>
                         {psn}
@@ -46,28 +46,27 @@ function handleCancel(){
                     }
                   </div>
                   <div>
-                  {PokeBaseStats.map(pbs=>(
-                  <Progress 
-                    trailColor='violet'
-                    percent={pbs/2} format={percent => `${pbs}`}
-                    status={"normal"}
-                  />
-                  ))
-                  }
+                    {PokeBaseStats.map(pbs=>(
+                    <Progress 
+                      trailColor='violet'
+                      percent={pbs/2} format={percent => `${pbs}`}
+                      status={"normal"}
+                    />
+                    ))
+                    }
                   </div>
                 </div>
-              </div>
-            </div>
-            <div>
-            {pType.map(pot => (
-              <p style={{background: `#${TYPE_COLORS[pot]}`, color: `#fff`, padding:'5px', textAlign:'center', fontSize:'25px', margin:'0'}}>
-                {pot}
-              </p>
-            ))}
-            </div> 
           </div>
-        </Modal>
-      </>
-    );
+        </div>
+        <div>
+              {pType.map(pot => (
+                <p className={s.p_type} style={{background: `#${TYPE_COLORS[pot]}`}}>
+                  {pot}
+                </p>
+              ))}
+        </div> 
+      </div>
+  </Modal>
+  );
 }
 
